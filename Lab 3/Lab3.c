3a325e2ea3a3d60415e61836e5d9be342066a465
ShivamVyas <shivam123qwe@hotmail.com>
2021-03-10 20:34:45 -0500

    Update Lab3.c

--- Lab 3/Lab3.c
+++ Lab 3/Lab3.c
@@ -9,10 +9,10 @@
 #define FILE_DELIM " "
 #define MAX_THREADS 16
 
-int  data[256][256] = { {0} };
+int  data[256][256] = {{0}};
 int  meshSize, threadIds[MAX_THREADS];
 bool threadWorkDone[MAX_THREADS] = {
-	0,
+    0,
 };
 pthread_t       threads[MAX_THREADS];
 pthread_mutex_t mutex_data, mutex_phaseComplete;
@@ -24,8 +24,8 @@ void bubblesort(int array[], int len, bool reverse) {
 	for (unsigned int i = 0; i < len - 1; i++) {
 		for (unsigned int j = 0; j < len - i - 1; j++) {
 			if (array[j + (reverse ? 1 : 0)] > array[j + (reverse ? 0 : 1)]) {
-				temp = array[j];
-				array[j] = array[j + 1];
+				temp         = array[j];
+				array[j]     = array[j + 1];
 				array[j + 1] = temp;
 			}
 		}
@@ -46,12 +46,12 @@ bool allThreadsComplete() {
 	return true;
 }
 
-void* meshThreadFunc(void* arg) {
+void *meshThreadFunc(void *arg) {
 	// printf("Thread #%i created\n", arg);
 	int arrayCopy[meshSize];
 	int threadNumber = (int)(uintptr_t)arg;
 
-	for (int phase = 0; phase <= ceil(log2(meshSize + 1.0)); phase++) {
+	for (int phase = 0; phase <= log2(meshSize)+1; phase++) {
 		if (phase % 2 == 0) { // Column sort
 			for (int i = 0; i < meshSize; i++)
 				arrayCopy[i] = data[i][threadNumber]; // Copy column data
@@ -64,16 +64,14 @@ void* meshThreadFunc(void* arg) {
 
 			pthread_mutex_unlock(&mutex_data);
 
-		}
-		else { // Row sort
+		} else { // Row sort
 			for (int i = 0; i < meshSize; i++)
 				arrayCopy[i] = data[threadNumber][i]; // Copy row data
 
 			if (threadNumber % 2 == 0) { // Increasing from left to right
 				bubblesort(arrayCopy, meshSize, false);
 
-			}
-			else { // Decreasing from left to right
+			} else { // Decreasing from left to right
 				bubblesort(arrayCopy, meshSize, true);
 			}
 
@@ -89,11 +87,10 @@ void* meshThreadFunc(void* arg) {
 		if (!allThreadsComplete()) { // If not all threads are complete, wait
 			pthread_mutex_lock(&mutex_phaseComplete);
 			pthread_cond_wait(&condition_phaseComplete,
-				&mutex_phaseComplete); // Wait for phases to complete
+			                  &mutex_phaseComplete); // Wait for phases to complete
 			pthread_mutex_unlock(&mutex_phaseComplete);
 
-		}
-		else {
+		} else {
 			for (size_t i = 0; i < meshSize; i++) threadWorkDone[i] = false; // Reset thread work
 
 			printf("Phase %i:\n", phase + 1);
@@ -106,14 +103,14 @@ void* meshThreadFunc(void* arg) {
 }
 
 int main(void) {
-	FILE* inputFile;
-	char           buf[255];
-	int            line;
+	FILE *inputFile;
+	char buf[255];
+	int  line;
 	pthread_attr_t attr;
 	inputFile = fopen("input.txt", "r");
 
-	for (line = 0; fgets(buf, sizeof(buf) / sizeof(char), (FILE*)inputFile) != NULL; line++) {
-		char* p = strtok(buf, FILE_DELIM);
+	for (line = 0; fgets(buf, sizeof(buf) / sizeof(char), (FILE *)inputFile) != NULL; line++) {
+		char *p = strtok(buf, FILE_DELIM);
 
 		for (int i = 0; p != NULL; i++) {
 			data[line][i] = atoi(p);
@@ -132,7 +129,7 @@ int main(void) {
 	printf("Unsorted matrix:\n");
 	printData();                                       // Print unsorted data
 	printf("\n");
-	pthread_mutex_init(&mutex_data, NULL);             // Init mutex for data
+  pthread_mutex_init(&mutex_data, NULL);             // Init mutex for data
 	pthread_mutex_init(&mutex_phaseComplete, NULL);    // Init mutex for phase complete
 	pthread_cond_init(&condition_phaseComplete, NULL); // Init condition for phase complete
 	pthread_attr_init(&attr);
@@ -140,16 +137,16 @@ int main(void) {
 
 	for (int i = 0; i < meshSize; i++) { // Create $(meshSize) threads
 		threadIds[i] = i;
-		pthread_create(&threads[i], &attr, meshThreadFunc, (void*)(uintptr_t)threadIds[i]);
+		pthread_create(&threads[i], &attr, meshThreadFunc, (void *)(uintptr_t)threadIds[i]);
 	}
 
 
 	pthread_attr_destroy(&attr);
 
 	for (int i = 0; i < meshSize; i++) { // Join all $(meshSize) threads
-		void* status;
+		void *status;
 		pthread_join(threads[i], &status);
-
+    
 		if (status != 0) {
 			printf("Thread #%i exited with %i\n", i, status);
 			return 1;
@@ -161,4 +158,4 @@ int main(void) {
 	pthread_mutex_destroy(&mutex_data); // Destroy mutex
 	pthread_exit(NULL);                 // Wait for all threads to stop execution
 	return 0;
-}
+} 
